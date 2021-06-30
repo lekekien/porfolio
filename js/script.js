4d02da30867087b2b3c5f4fb08514e9231b117b6
lekekien <40720438+lekekien@users.noreply.github.com>
2021-06-30 17:46:37 +0700

    lkkien commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-$('#gallery').mixItUp({});
+$(document).ready(function(){
+    $('#gallery').mixItUp({});
+
+    var header = $("#menu-nav");
+    window.onscroll = function(){
+        console.log("win :" + window.pageYOffset)
+        console.log($("#menu-nav").offset().top)
+        if(window.pageYOffset >= $("#menu-nav").offset().top){
+            header.addClass("sticky")
+        }
+        else{
+            header.removeClass("sticky")
+        }
+    }
+})

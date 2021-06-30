242fc1c426d7e8fc46ae22e56ad650134179f9c6
kienlk <humg271196@gmail.com>
2021-07-01 01:07:51 +0700

    lkkirn commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
-$(document).ready(function(){
+$(function(){
     $('#gallery').mixItUp({});
-
     var navPos = $("#menu-nav").position().top;
     var lastPos = 0;
-    window.onscroll = function(){
+    checkFixedWhenScroll();
+    $(window).on("scroll",function(){
+        checkFixedWhenScroll();
+    })
+
+    $(".link-item").each(function(){
+        var cur = $(this);
+        cur.on("click", function(){
+            var target = cur.data("target");
+            $('html, body').animate({
+                scrollTop: $("[data-link="+target+"]").offset().top
+            }, 500);
+            highlightLink(target);
+        });
+    })
+    function highlightLink(anchor) {
+        $('.link-item.active').removeClass('active');
+        $('.menu-nav')
+            .find('[data-target="' + anchor + '"]')
+            .addClass('active');
+    }
+    function checkFixedWhenScroll(){
+       
         var pos = $(window).scrollTop();
-        if (pos >= navPos + $("#menu-nav").height() && lastPos < pos) {
-            $("#menu-nav").addClass('fixed');
+        var pos2 = pos + 250;
+        if (pos >= navPos  && lastPos < pos) {
+            $("#menu-nav").addClass('sticky');
         }
         if (pos < navPos && lastPos > pos) {
-            $("#menu-nav").removeClass('fixed');
-        }
+            $("#menu-nav").removeClass('sticky');
+        }  
         lastPos = pos;
+    
+        if (pos2 > $('#home').offset().top) {
+            highlightLink('home');
+        }
+        if (pos2 > $('#about').offset().top) {
+            highlightLink('about');
+        }
+        if (pos2 > $('#project').offset().top) {
+            highlightLink('project');
+        }
     }
+    
 })
+
